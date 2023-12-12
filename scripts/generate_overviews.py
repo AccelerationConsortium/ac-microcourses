94e1871d08ed1384bc737df1b13a59e3924ca7cf
Sterling G. Baird <sterling.baird@utoronto.ca>
2023-12-12 12:19:31 -0500

    fix relative path issue

--- scripts/generate_overviews.py
+++ scripts/generate_overviews.py
@@ -1,12 +1,21 @@
+import os
+
 import yaml
 from jinja2 import Environment, FileSystemLoader
 
+# Get the directory containing this script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
 # Load the YAML file
-with open("docs/course-data.yaml", "r", encoding="utf-8") as f:
+with open(
+    os.path.join(script_dir, "../docs/course-data.yaml"), "r", encoding="utf-8"
+) as f:
     courses = yaml.load(f, Loader=yaml.FullLoader)
 
 # Define the Jinja2 environment and load the template
-env = Environment(loader=FileSystemLoader("src/ac_microcourses"))
+env = Environment(
+    loader=FileSystemLoader(os.path.join(script_dir, "../src/ac_microcourses"))
+)
 template = env.get_template("overview.md.jinja")
 
 # For each course, render the template and write the result to a markdown file
@@ -24,7 +33,11 @@ for course_name, course_data in courses.items():
         # e.g., replace {{ hello-world }} with title of hello-world course
 
     # Write the markdown to a file
-    with open(f"docs/courses/{course_name}/overview.md", "w", encoding="utf-8") as f:
+    with open(
+        os.path.join(script_dir, f"../docs/courses/{course_name}/overview.md"),
+        "w",
+        encoding="utf-8",
+    ) as f:
         f.write(markdown)
 
 1 + 1
