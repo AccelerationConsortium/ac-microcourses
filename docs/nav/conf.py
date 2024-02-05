c9a2bc74a597ed96a70cf57fd6d9ab2cd02fcf20
Sterling G. Baird <sterling.baird@utoronto.ca>
2024-02-05 14:03:19 -0500

    nav site

--- docs/nav/conf.py
+++ docs/nav/conf.py
@@ -165,7 +165,7 @@ source_suffix = [".rst", ".md"]
 # source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = "../index"
+master_doc = "index"
 
 # General information about the project.
 project = "ac-microcourses"
