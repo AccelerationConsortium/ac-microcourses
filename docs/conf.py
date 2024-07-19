fc9631655bdb4104289b17e5435fdbb7b3eb70fb
Sterling G. Baird <sterling.baird@utoronto.ca>
2024-07-19 12:18:32 -0400

    dual copyright

--- docs/conf.py
+++ docs/conf.py
@@ -185,7 +185,7 @@ master_doc = "index"
 # General information about the project.
 project = "ac-microcourses"
 author = "Sterling G. Baird"
-copyright = "2023, Sterling G. Baird"
+copyright = ["2023, Acceleration Consortium", "2023, Sterling G. Baird"]
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
