2dda1826af987a75ebc702543a8b4474505dfb03
Sterling G. Baird <sterling.baird@utoronto.ca>
2025-02-26 23:57:02 -0500

    naming

--- docs/courses/hello-world/write_mongodb.py
+++ docs/courses/hello-world/write_mongodb.py
@@ -1,14 +1,14 @@
 import json  # noqa: F401
 
 import requests
-from my_secrets import ATLAS_URI, COURSE_ID, PASSWORD, SSID
+from my_secrets import COURSE_ID, LAMBDA_FUNCTION_URL, PASSWORD, SSID
 from netman import connectWiFi
 
 connectWiFi(SSID, PASSWORD, country="US")
 
 document = {"course_id": COURSE_ID}
 
-response = requests.post(ATLAS_URI, json=document)
+response = requests.post(LAMBDA_FUNCTION_URL, json=document)
 
 if response.status_code == 200:
     response_data = response.json()
